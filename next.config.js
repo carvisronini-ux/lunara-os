50bc0a757fd1358b2978a7b05976134787230839
carvisronini-ux <carvisronini@gmail.com>
2026-09-16 14:17:37 +0400

    1

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      serverActions: true,
-    },
-    env: {
-      NEXT_PUBLIC_SUPABASE_OS_URL: process.env.NEXT_PUBLIC_SUPABASE_OS_URL,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**.supabase.co',
-        },
-      ],
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+
+  env: {
+    NEXT_PUBLIC_SUPABASE_OS_URL:
+      process.env.NEXT_PUBLIC_SUPABASE_OS_URL,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
